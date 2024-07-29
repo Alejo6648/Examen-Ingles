636d0453cc1082dfa3ed2da2275366caa1fa0d96
Alejo6648 <alorgomez2006@gmail.com>
2024-07-29 12:04:30 -0500

    cambios

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./index.html', './src/**/*.{js,ts,vue,html}'], // Ajusta las rutas según tus archivos
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
